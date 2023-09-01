b4a12f9d5b1e25bb8b9d1bc2ca41aabb6917704a
RewanKhaled <rewankhaled219@gmail.com>
2023-09-01 13:48:05 +0300

    Task1 commit

---
+++ Task1/number1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //Print the string 'GRU'
+    printf("GRU\n");
+    printf("\n");
+
+    //Draw 'GRU' using asterisks
+    printf("*****   ****   *    *\n");
+    printf("*       *   *  *    *\n");
+    printf("*  **   ****   *    *\n");
+    printf("*   *   *   *  *    *\n");
+    printf("*****   *    * ******\n");
+    return 0;
+}
